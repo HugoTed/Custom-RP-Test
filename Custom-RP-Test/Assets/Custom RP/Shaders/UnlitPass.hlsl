59000044d2f45a50d0d0231fe21856a08aee7f00
zarkfair <zarkfair@live.com>
2022-09-12 23:57:25 +0800

    Add draw call

---
+++ Custom-RP-Test/Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -0,0 +1,20 @@
+﻿//限制范围
+#ifndef CUSTOM_UNLIT_PASS_INCLUDED
+#define CUSTOM_UNLIT_PASS_INCLUDED
+
+#include "../ShaderLibrary/Common.hlsl"
+
+float4 _BaseColor;
+
+float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION 
+{
+	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+	float4 positionCS = TransformWorldToHClip(positionWS);
+	return positionCS;
+}
+float4 UnlitPassFragment(): SV_TARGET
+{
+	return _BaseColor;
+}
+
+#endif
