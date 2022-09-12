a623be4169219013370cb83bb01c5d948d36b1eb
zarkfair <zarkfair@live.com>
2022-09-12 20:44:25 +0800

    add Custom SRP

---
+++ Custom-RP-Test/Assets/Custom RP/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using UnityEngine.Profiling;
+
+partial class CameraRenderer
+{
+    string SampleName { get; set; }
+    partial void DrawGizmos();
+    partial void DrawUnsupportShaders();
+
+    partial void PrepareForSceneWindow();
+
+    partial void PrepareBuffer();
+#if UNITY_EDITOR
+
+    static Material errorMaterial;
+
+    //旧版的着色器就算不支持了也要渲染出来
+    static ShaderTagId[] legacyShaderTagIds = new ShaderTagId[]
+    {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM"),
+    };
+
+    //渲染Gizmos
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+    //渲染不支持的shader
+    partial void DrawUnsupportShaders()
+    {
+        if(errorMaterial == null)
+        {
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
+        var drawingSettings = new DrawingSettings(legacyShaderTagIds[0], new SortingSettings(camera))
+        {
+            overrideMaterial = errorMaterial
+        };
+
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        var filteringSettings = FilteringSettings.defaultValue;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    //编辑器场景中渲染ui
+    partial void PrepareForSceneWindow()
+    {
+        if(camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+    //给不同摄像机的buffer定义不同的名称
+    partial void PrepareBuffer()
+    {
+        Profiler.BeginSample("Editor Only");
+        buffer.name = SampleName = camera.name;
+        Profiler.EndSample();
+    }
+#else
+    string SampleName => bufferName;
+#endif
+}
---
+++ Custom-RP-Test/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer() { name = bufferName };
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context,Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+        PrepareBuffer();
+        //在剔除之前完成UI
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportShaders();
+        DrawGizmos();
+        Sumbit();
+    }
+
+    void Setup()
+    {
+        //设置视图投影矩阵
+        context.SetupCameraProperties(camera);
+        //CameraClearFlags枚举定义了四个值。从 1 到 4，它们是Skybox、Color、Depth和Nothing。
+        
+        CameraClearFlags flags = camera.clearFlags;
+        //除了Nothing值，在flags的值不大于Depth的所有情况下都必须清除深度缓冲区(depth buffer)。
+        //渲染之前清除旧内容
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color?
+                camera.backgroundColor.linear:Color.clear
+            );
+
+        //使其出现在frame debugger中
+        buffer.BeginSample(SampleName);
+
+        ExecuteBuffer();
+    }
+
+    void Sumbit()
+    {
+        
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        //执行缓冲区
+        context.ExecuteCommandBuffer(buffer);
+        //执行完清除
+        buffer.Clear();
+    }
+
+    void DrawVisibleGeometry()
+    {
+        //对可见对象进行排序，以及要调用的shader pass
+        var sortingSettings = new SortingSettings(camera)
+        {
+            //强制渲染顺序
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        //允许所有渲染队列
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        //调用剔除结果作为参数进行渲染
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        context.DrawSkybox(camera);
+
+        //先渲染不透明物体，再渲染天空盒，再由远到近渲染透明物体
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    bool Cull()
+    {
+        //尝试剔除
+        if(camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            //是否成功检索参数
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
